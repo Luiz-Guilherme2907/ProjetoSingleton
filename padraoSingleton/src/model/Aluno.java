d46f3d19c8f843d613b73df4a9c49669ed8c3c47
Luiz-Guilherme2907 <luizguilhermemodler2907@gmail.com>
2024-09-27 20:15:44 -0300

    Adicionado as classes (pessoa, aluno, professor)

--- padraoSingleton/src/model/Aluno.java
+++ padraoSingleton/src/model/Aluno.java
@@ -1,4 +1,21 @@
 package model;
 
-public class Aluno {
+public class Aluno extends Pessoa{
+    private String matricula;
+    private String curso;
+    
+   private static Aluno instance;
+
+    Aluno(){
+        super();
+        this.matricula = "";
+        this.curso = "";
+    }
+
+    public static Aluno getInstance() {
+        if (instance == null) {
+            instance = new Aluno();
+        }
+        return instance;
+    }
 }
